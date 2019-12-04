ac065f81ee1eaf5603314588af58053dcfe0c650
mac <you@example.com>
2019-12-04 22:53:50 +0600

    Settings changed

--- ToDoListApp/settings.py
+++ ToDoListApp/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku 
-django_heroku.settings(locals())
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 MEDIA_DIR = os.path.join(BASE_DIR,'media')
@@ -28,7 +31,7 @@ SECRET_KEY = 'd3vy^4hczr&c4k@vm3q%e5c)037ru*(tgti_k_5r65^-^%(^-c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,14 +123,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
@@ -136,3 +140,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # media format
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
