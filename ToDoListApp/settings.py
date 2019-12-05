ac1531cfcfd9fe7af9863c7fad673a1a87dc8507
mac <you@example.com>
2019-12-06 00:40:46 +0600

    STATIC_ROOT changed

--- ToDoListApp/settings.py
+++ ToDoListApp/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
